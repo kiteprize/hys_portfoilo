8f2fd42c7d3608b191ef69dadd9ea3676dd99a8e
황연상 <hwangyeonsang@hwang-yeonsangs-MacBook-Air.local>
2022-07-07 18:15:52 +0900

    Epic 1 end

--- src/Epic_01.js
+++ src/Epic_01.js
@@ -4,13 +4,6 @@ import './App.css';
 import IntroduceLine  from './Introduce';
 
 function App() {
-  const setScreenSize = () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  }
-  useEffect(() => {
-    window.addEventListener('resize', () => setScreenSize());
-  });
   return (
     <div className="App">
       <BrowserView>
