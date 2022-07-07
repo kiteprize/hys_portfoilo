cb64e206718463f278a95d0c51595b4877e9f08a
황연상 <hwangyeonsang@hwang-yeonsangs-MacBook-Air.local>
2022-07-07 22:34:01 +0900

    Epic 1 end

--- src/Epic_01.js
+++ src/Epic_01.js
@@ -35,7 +35,7 @@ class IntroduceLine extends Component{
 function App() {
   const setScreenSize = () => {
     let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', '${vh}px');
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
   }
   useEffect(() => {
     setScreenSize();
