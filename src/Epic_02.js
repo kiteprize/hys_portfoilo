faca195fb48ee56af9653bd665610927c3865cf5
황연상 <hwangyeonsang@hwang-yeonsangs-MacBook-Air.local>
2022-07-11 23:03:41 +0900

    Epic 2 End

--- src/Epic_02.js
+++ src/Epic_02.js
@@ -8,27 +8,47 @@ function App(){
         <div>
             <BrowserView>
                 <div className="background">
-                    <div className="Maintext">
+                    <div className="Browser-Maintext">
                         SKILLS
                     </div>
                     <div className="Browser-Skillbox">
+                        <div className="Browser-Skillbox-Header">
+                            FRONT END
+                        </div>
+                        <div className="Browser-Skillbox-Header-UnderLine"/>
+                        <img className="Browser-Skillbox-icon" src="Skill_icon/css.png" alt="css"/>
+                        <img className="Browser-Skillbox-icon" src="Skill_icon/html.png" alt="html"/>
+                        <img className="Browser-Skillbox-icon" src="Skill_icon/javascript.png" alt="javascript"/>
+                        <img className="Browser-Skillbox-icon" src="Skill_icon/react.png" alt="react"/>
                     </div>
                     <div className="Browser-Skillbox">
+                        <div className="Browser-Skillbox-Header">
+                            TOOLS
+                        </div>
+                        <div className="Browser-Skillbox-Header-UnderLine"/>
+                        <img className="Browser-Skillbox-icon" src="Skill_icon/vscode.png" alt="vscode"/>
+                        <img className="Browser-Skillbox-icon" src="Skill_icon/eclipse.png" alt="eclipse"/>
                     </div>
                     <div className="Browser-Skillbox">
+                        <div className="Browser-Skillbox-Header">
+                            COMMUNICATION
+                        </div>
+                        <div className="Browser-Skillbox-Header-UnderLine"/>
+                        <img className="Browser-Skillbox-icon" src="Skill_icon/slack.png" alt="slack"/>
+                        <img className="Browser-Skillbox-icon" src="Skill_icon/jira.png" alt="jira"/>
                     </div>
                 </div>
             </BrowserView>
             <MobileView>
                 <div className="background">
-                    <div className="Maintext">
+                    <div className="Mobile-Maintext">
                         SKILLS
                     </div>
                     <div className="Mobile-Skillbox">
                         <div className="Mobile-Skillbox-Header">
                             FRONT END
                         </div>
-                        <div className="Header-Underline"/>
+                        <div className="Mobile-Skillbox-Header-UnderLine"/>
                         <img className="Mobile-Skillbox-icon" src="Skill_icon/css.png" alt="css"/>
                         <img className="Mobile-Skillbox-icon" src="Skill_icon/html.png" alt="html"/>
                         <img className="Mobile-Skillbox-icon" src="Skill_icon/javascript.png" alt="javascript"/>
@@ -38,7 +58,7 @@ function App(){
                         <div className="Mobile-Skillbox-Header">
                             TOOLS
                         </div>
-                        <div className="Header-Underline"/>
+                        <div className="Mobile-Skillbox-Header-UnderLine"/>
                         <img className="Mobile-Skillbox-icon" src="Skill_icon/vscode.png" alt="vscode"/>
                         <img className="Mobile-Skillbox-icon" src="Skill_icon/eclipse.png" alt="eclipse"/>
                     </div>
@@ -46,7 +66,7 @@ function App(){
                         <div className="Mobile-Skillbox-Header">
                             COMMUNICATION
                         </div>
-                        <div className="Header-Underline"/>
+                        <div className="Mobile-Skillbox-Header-UnderLine"/>
                         <img className="Mobile-Skillbox-icon" src="Skill_icon/slack.png" alt="slack"/>
                         <img className="Mobile-Skillbox-icon" src="Skill_icon/jira.png" alt="jira"/>
                     </div>
