a4406ddae655ef9ed52f0531d4543d93fd781f0c
kiteprize <dustkd4294@naver.com>
2022-07-14 17:17:52 +0900

    Epic 3 Card End

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StyledEngineProvider } from '@mui/material/styles';
 import Epic01 from './Epic_01';
 import Epic02 from './Epic_02';
 import Epic03 from './Epic_03';
@@ -10,8 +9,6 @@ root.render(
   <React.StrictMode>
     <Epic01 />
     <Epic02 />
-    <StyledEngineProvider injectFirst>
-      <Epic03 />
-    </StyledEngineProvider>
+    <Epic03 />
   </React.StrictMode>
 );
