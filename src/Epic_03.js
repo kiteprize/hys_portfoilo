27cb7f87b4d370b0ee042ad979143e6d9246b6fc
kiteprize <dustkd4294@naver.com>
2022-07-14 16:34:54 +0900

    Epic 3 Card End

--- src/Epic_03.js
+++ src/Epic_03.js
@@ -8,14 +8,42 @@ function App(){
             <BrowserView>
                 <div className="background">
                     <div className="Browser-Maintext">
-                        PROJECT
+                        PROJECTS
+                    </div>
+                    <div className="Browser-Card">
+                        <img className="Browser-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
+                        <div className="Browser-Card-Title">
+                            개인 포트폴리오
+                        </div>
+                        <div className="Browser-Card-SubTitle">
+                            react를 사용한 반응형 포트폴리오 페이지
+                        </div>
+                        <div className="Browser-Card-Detail">
+                            <div className="Browser-Card-Detail-Text">
+                                자세히 보기
+                            </div>
+                        </div>
                     </div>
                 </div>
             </BrowserView>
             <MobileView>
                 <div className="background">
                     <div className="Mobile-Maintext">
-                        PROJECT
+                        PROJECTS
+                    </div>
+                    <div className="Mobile-Card">
+                        <img className="Mobile-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
+                        <div className="Mobile-Card-Title">
+                            개인 포트폴리오 사이트
+                        </div>
+                        <div className="Mobile-Card-SubTitle">
+                            React를 사용한 반응형 포트폴리오 페이지
+                        </div>
+                        <div className="Mobile-Card-Detail">
+                            <div className="Mobile-Card-Detail-Text">
+                                자세히 보기
+                            </div>
+                        </div>
                     </div>
                 </div>
             </MobileView>
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
 import Epic01 from './Epic_01';
 import Epic02 from './Epic_02';
 import Epic03 from './Epic_03';
@@ -9,6 +10,8 @@ root.render(
   <React.StrictMode>
     <Epic01 />
     <Epic02 />
-    <Epic03 />
+    <StyledEngineProvider injectFirst>
+      <Epic03 />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
