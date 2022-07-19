9e04a50a875ea4158415fa4f82926b7eedce007a
kiteprize <dustkd4294@naver.com>
2022-07-19 18:00:17 +0900

    slick test

--- src/Epic_03.js
+++ src/Epic_03.js
@@ -1,8 +1,21 @@
 import React from "react";
 import {BrowserView, MobileView} from 'react-device-detect';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import 'slick-carousel/slick/slick-theme.css';
 import './Epic_03.css';
 
 function App(){
+    const settings ={
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dragable: true,
+    };
+
     return(
         <div>
             <BrowserView>
@@ -10,17 +23,45 @@ function App(){
                     <div className="Browser-Maintext">
                         PROJECTS
                     </div>
-                    <div className="Browser-Card">
-                        <img className="Browser-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
-                        <div className="Browser-Card-Title">
-                            개인 포트폴리오 사이트
-                        </div>
-                        <div className="Browser-Card-SubTitle">
-                            react를 사용한 반응형 포트폴리오 페이지
-                        </div>
-                        <div className="Browser-Card-Detail">
-                            자세히 보기
-                        </div>
+                    <div className="browser-container">
+                        <Slider {...settings}>
+                            <div className="Browser-Card">
+                                <img className="Browser-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
+                                <div className="Browser-Card-Title">
+                                    개인 포트폴리오 사이트
+                                </div>
+                                <div className="Browser-Card-SubTitle">
+                                    React를 사용한 반응형 포트폴리오 페이지
+                                </div>
+                                <div className="Browser-Card-Detail">
+                                    자세히 보기
+                                </div>
+                            </div>
+                            <div className="Browser-Card">
+                                <img className="Browser-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
+                                <div className="Browser-Card-Title">
+                                    개인 포트폴리오 사이트
+                                </div>
+                                <div className="Browser-Card-SubTitle">
+                                    React를 사용한 반응형 포트폴리오 페이지
+                                </div>
+                                <div className="Browser-Card-Detail">
+                                    자세히 보기
+                                </div>
+                            </div>
+                            <div className="Browser-Card">
+                                <img className="Browser-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
+                                <div className="Browser-Card-Title">
+                                    개인 포트폴리오 사이트
+                                </div>
+                                <div className="Browser-Card-SubTitle">
+                                    React를 사용한 반응형 포트폴리오 페이지
+                                </div>
+                                <div className="Browser-Card-Detail">
+                                    자세히 보기
+                                </div>
+                            </div>
+                        </Slider>
                     </div>
                 </div>
             </BrowserView>
@@ -29,17 +70,45 @@ function App(){
                     <div className="Mobile-Maintext">
                         PROJECTS
                     </div>
-                    <div className="Mobile-Card">
-                        <img className="Mobile-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
-                        <div className="Mobile-Card-Title">
-                            개인 포트폴리오 사이트
-                        </div>
-                        <div className="Mobile-Card-SubTitle">
-                            React를 사용한 반응형 포트폴리오 페이지
-                        </div>
-                        <div className="Mobile-Card-Detail">
-                            자세히 보기
-                        </div>
+                    <div className="mobile-container">
+                        <Slider {...settings}>
+                            <div className="Mobile-Card">
+                                <img className="Mobile-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
+                                <div className="Mobile-Card-Title">
+                                    개인 포트폴리오 사이트
+                                </div>
+                                <div className="Mobile-Card-SubTitle">
+                                    React를 사용한 반응형 포트폴리오 페이지
+                                </div>
+                                <div className="Mobile-Card-Detail">
+                                    자세히 보기
+                                </div>
+                            </div>
+                            <div className="Mobile-Card">
+                                <img className="Mobile-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
+                                <div className="Mobile-Card-Title">
+                                    개인 포트폴리오 사이트
+                                </div>
+                                <div className="Mobile-Card-SubTitle">
+                                    React를 사용한 반응형 포트폴리오 페이지
+                                </div>
+                                <div className="Mobile-Card-Detail">
+                                    자세히 보기
+                                </div>
+                            </div>
+                            <div className="Mobile-Card">
+                                <img className="Mobile-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
+                                <div className="Mobile-Card-Title">
+                                    개인 포트폴리오 사이트
+                                </div>
+                                <div className="Mobile-Card-SubTitle">
+                                    React를 사용한 반응형 포트폴리오 페이지
+                                </div>
+                                <div className="Mobile-Card-Detail">
+                                    자세히 보기
+                                </div>
+                            </div>
+                        </Slider>
                     </div>
                 </div>
             </MobileView>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import Epic01 from './Epic_01';
 import Epic02 from './Epic_02';
 import Epic03 from './Epic_03';
+// import Epic04 from './Epic_04';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,5 +11,6 @@ root.render(
     <Epic01 />
     <Epic02 />
     <Epic03 />
+    {/* <Epic04 /> */}
   </React.StrictMode>
 );
