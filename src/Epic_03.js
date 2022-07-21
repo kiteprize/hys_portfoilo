f5c02fbd2cb37ea884b188b6e005698c2feae923
kiteprize <dustkd4294@naver.com>
2022-07-21 17:13:56 +0900

    functionalize test

--- src/Epic_03.js
+++ src/Epic_03.js
@@ -7,6 +7,40 @@ import "slick-carousel/slick/slick.css";
 import 'slick-carousel/slick/slick-theme.css';
 import './Epic_03.css';
 
+function BrowserCard(props){
+    return(
+        <div className="Browser-Card">
+            <img className="Browser-Card-Image" src={props.img} alt="PortFolio_MainPage"/>
+            <div className="Browser-Card-Title">
+                {props.Tilte}
+            </div>
+            <div className="Browser-Card-SubTitle">
+                {props.SubTitle}
+            </div>
+            <div className="Browser-Card-Detail" onClick={props.openDrawer}>
+                자세히 보기
+            </div>
+        </div>
+    );
+}
+
+function MobileCard(props){
+    return(
+        <div className="Mobile-Card">
+            <img className="Mobile-Card-Image" src={props.img} alt="Thumbnail"/>
+            <div className="Mobile-Card-Title">
+                {props.Tilte}
+            </div>
+            <div className="Mobile-Card-SubTitle">
+                {props.SubTitle}
+            </div>
+            <div className="Mobile-Card-Detail" onClick={props.openDrawer}>
+                자세히 보기
+            </div>
+        </div>
+    );
+}
+
 function App(){
     const [isVisible, setIsVisible] = React.useState(false);
     const openDrawer = React.useCallback(() => setIsVisible(true), []);
@@ -30,42 +64,24 @@ function App(){
                     </div>
                     <div className="browser-container">
                         <Slider {...settings}>
-                            <div className="Browser-Card">
-                                <img className="Browser-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
-                                <div className="Browser-Card-Title">
-                                    개인 포트폴리오 사이트
-                                </div>
-                                <div className="Browser-Card-SubTitle">
-                                    React를 사용한 반응형 포트폴리오 페이지
-                                </div>
-                                <div className="Browser-Card-Detail">
-                                    자세히 보기
-                                </div>
-                            </div>
-                            <div className="Browser-Card">
-                                <img className="Browser-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
-                                <div className="Browser-Card-Title">
-                                    개인 포트폴리오 사이트
-                                </div>
-                                <div className="Browser-Card-SubTitle">
-                                    React를 사용한 반응형 포트폴리오 페이지
-                                </div>
-                                <div className="Browser-Card-Detail">
-                                    자세히 보기
-                                </div>
-                            </div>
-                            <div className="Browser-Card">
-                                <img className="Browser-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
-                                <div className="Browser-Card-Title">
-                                    개인 포트폴리오 사이트
-                                </div>
-                                <div className="Browser-Card-SubTitle">
-                                    React를 사용한 반응형 포트폴리오 페이지
-                                </div>
-                                <div className="Browser-Card-Detail">
-                                    자세히 보기
-                                </div>
-                            </div>
+                            <BrowserCard 
+                                img = "portfolio_thumbnail/portfolio.png" 
+                                Tilte = "개인 포트폴리오 사이트" 
+                                SubTitle = "React를 사용한 반응형 포트폴리오 페이지"
+                                openDrawer = {openDrawer}
+                            />
+                            <BrowserCard 
+                                img = "portfolio_thumbnail/portfolio.png" 
+                                Tilte = "개인 포트폴리오 사이트" 
+                                SubTitle = "React를 사용한 반응형 포트폴리오 페이지"
+                                openDrawer = {openDrawer}
+                            />
+                            <BrowserCard 
+                                img = "portfolio_thumbnail/portfolio.png" 
+                                Tilte = "개인 포트폴리오 사이트" 
+                                SubTitle = "React를 사용한 반응형 포트폴리오 페이지"
+                                openDrawer = {openDrawer}
+                            />
                         </Slider>
                     </div>
                 </div>
@@ -77,42 +93,24 @@ function App(){
                     </div>
                     <div className="mobile-container">
                         <Slider {...settings}>
-                            <div className="Mobile-Card">
-                                <img className="Mobile-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
-                                <div className="Mobile-Card-Title">
-                                    개인 포트폴리오 사이트
-                                </div>
-                                <div className="Mobile-Card-SubTitle">
-                                    React를 사용한 반응형 포트폴리오 페이지
-                                </div>
-                                <div className="Mobile-Card-Detail" onClick={openDrawer}>
-                                    자세히 보기
-                                </div>
-                            </div>
-                            <div className="Mobile-Card">
-                                <img className="Mobile-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
-                                <div className="Mobile-Card-Title">
-                                    개인 포트폴리오 사이트
-                                </div>
-                                <div className="Mobile-Card-SubTitle">
-                                    React를 사용한 반응형 포트폴리오 페이지
-                                </div>
-                                <div className="Mobile-Card-Detail" onClick={openDrawer}>
-                                    자세히 보기
-                                </div>
-                            </div>
-                            <div className="Mobile-Card">
-                                <img className="Mobile-Card-Image" src="portfolio_thumbnail/portfolio.png" alt="PortFolio_MainPage"/>
-                                <div className="Mobile-Card-Title">
-                                    개인 포트폴리오 사이트
-                                </div>
-                                <div className="Mobile-Card-SubTitle">
-                                    React를 사용한 반응형 포트폴리오 페이지
-                                </div>
-                                <div className="Mobile-Card-Detail" onClick={openDrawer}>
-                                    자세히 보기
-                                </div>
-                            </div>
+                            <MobileCard 
+                            img = "portfolio_thumbnail/portfolio.png" 
+                            Tilte = "개인 포트폴리오 사이트" 
+                            SubTitle = "React를 사용한 반응형 포트폴리오 페이지"
+                            openDrawer = {openDrawer}
+                            />
+                            <MobileCard 
+                            img = "portfolio_thumbnail/portfolio.png" 
+                            Tilte = "개인 포트폴리오 사이트" 
+                            SubTitle = "React를 사용한 반응형 포트폴리오 페이지"
+                            openDrawer = {openDrawer}
+                            />
+                            <MobileCard 
+                            img = "portfolio_thumbnail/portfolio.png" 
+                            Tilte = "개인 포트폴리오 사이트" 
+                            SubTitle = "React를 사용한 반응형 포트폴리오 페이지"
+                            openDrawer = {openDrawer}
+                            />
                         </Slider>
                         <Drawer
                                 duration={400}
@@ -121,8 +119,8 @@ function App(){
                                 isVisible={isVisible}
                                 className={"drawer"}
                             >
-                                <Epic04 />
-                            </Drawer>
+                            <Epic04 />
+                        </Drawer>
                     </div>
                 </div>
             </MobileView>
